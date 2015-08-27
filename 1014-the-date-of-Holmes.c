005273f74c932942f5d6eeda0938200a86c98825
Death <lkwq007@gmail.com>
2015-08-27 14:26:19 +0800

    Update 1014

--- 1014-the-date-of-Holmes.c
+++ 1014-the-date-of-Holmes.c
@@ -1,13 +1,41 @@
 #include<stdio.h>
 int main()
 {
-	char str[4][61];
-	int i,j,count=0;
+	char str[4][61],day[7][4]={"MON","TUE","WED","THU","FRI","SAT","SUN"};
+	int i,count=0;
 	for(i=0;i<4;i++)
 		scanf("%s",str[i]);
-	for(i=0;i<2;i++)
+	i=0;
+	while(str[0][i]!='\0'&&str[1][i]!='\0')
 	{
-		j=0;
-		while()
+		if(str[0][i]==str[1][i]&&(str[0][i]>='0'&&str[0][i]<='9'||str[0][i]>='A'&&str[0][i]<='N'))
+		{
+			if(count==0)
+			{
+				if(str[0][i]>='A'&&str[0][i]<='G')
+				{
+					printf("%s ",day[str[0][i]-'A']);
+					count++;
+				}
+			}
+			else if(count==1)
+			{
+				printf("%02d:",str[0][i]>='0'&&str[0][i]<='9'?str[0][i]-'0':str[0][i]-'A'+10);
+				count++;
+			}
+			else
+				break;
+		}
+		i++;
+	}
+	i=0;
+	while(str[2][i]!='\0'&&str[3][i]!='\0')
+	{
+		if(str[2][i]==str[3][i]&&(str[2][i]>='a'&&str[2][i]<='z'||str[2][i]>='A'&&str[2][i]<='Z'))
+		{
+			printf("%02d",i);
+			break;
+		}
+		i++;
 	}
 }
