9155d226bd314df1e815f5b006560d5e42a7eb77
Death <lkwq007@gmail.com>
2015-08-27 18:59:52 +0800

    Update 1017

--- 1017-A-divide-by-B.c
+++ 1017-A-divide-by-B.c
@@ -2,31 +2,23 @@
 
 int main()
 {
-	char a[1001],*pa=a,temp,q[1001],*pq=q;
+	char a[1001],*pa=a,temp[3],q[1001],*pq=q;
 	int b,r=0,num;
 	scanf("%s%d",a,&b);
 	while(*pa)
 	{
-		if(*pa-'0'>=b)
-		{
-			*(pq++)=(*pa-'0')/b+'0';
-			r=(*pa-'0')%b;
-		}
-		else
-		{
-			if(*(pa+1)!='\0')
-			{
-				temp=*(pa+2);
-				*(pa+2)='\0';
-				*pa+=r;
-				sscanf(pa,"%d",&num);
-				*(pa+2)=temp;
-				*(pq++)=num/b+'0';
-				r=num%b;
-			}
-		}
+		temp[0]=r+'0';
+		temp[1]=*pa;
+		temp[2]='\0';
+		sscanf(temp,"%d",&num);
+		*(pq++)=num/b+'0';
+		r=num%b;
 		pa++;
 	}
 	*pq='\0';
-	printf("%s %d",q,r);
+	if(q[0]=='0'&&q[1]=='\0'||q[0]!='0')
+		pq=q;
+	else
+		pq=q+1;
+	printf("%s %d",pq,r);
 }
