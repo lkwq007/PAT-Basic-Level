6248809abafa33a656bd54adb4c9f692964926e2
Death <lkwq007@gmail.com>
2015-08-28 10:36:23 +0800

    Update 1020

--- 1020-mooncake.c
+++ 1020-mooncake.c
@@ -6,17 +6,25 @@ typedef struct mooncake
 	int sale;
 	double price;
 } MOONCAKE;
-int rcompare(const void *a,const void *b)
+/*int rcompare(const void *a,const void *b)
 {
 	if(((MOONCAKE *)b)->price>((MOONCAKE *)a)->price)
 		return 1;
 	else if(((MOONCAKE *)b)->price<((MOONCAKE *)a)->price)
 		return -1;
 	return 0;
+}*/
+int rcompare(const void *a,const void *b)
+{
+	if(((MOONCAKE *)b)->sale*((MOONCAKE *)a)->amount>((MOONCAKE *)a)->sale*((MOONCAKE *)b)->amount)
+		return 1;
+	else if(((MOONCAKE *)b)->sale*((MOONCAKE *)a)->amount<((MOONCAKE *)a)->sale*((MOONCAKE *)b)->amount)
+		return -1;
+	return 0;
 }
 int main()
 {
-	int i,n,demand,amount=0;
+	int i,n,demand,amount=0,enough=0;
 	double result=0;
 	MOONCAKE *mc;
 	scanf("%d%d",&n,&demand);
@@ -26,23 +34,27 @@ int main()
 	for(i=0;i<n;i++)
 	{
 		scanf("%d",&(mc[i].sale));
-		mc[i].price=mc[i].sale/(double)mc[i].amount;
+		//mc[i].price=mc[i].sale/(double)mc[i].amount;
 	}
 	qsort(mc,n,sizeof(MOONCAKE),rcompare);
 	for(i=0;i<n;i++)
 	{
 		if(amount>=demand)
+		{
+			enough=1;
 			break;
+		}
 		amount+=(mc[i].amount);
 	}
 	n=i;
 	amount=0;
 	for(i=0;i<n;i++)
 	{
-		if(i==n-1)
+		if(i==n-1&&enough==1)
 		{
 			amount=demand-amount;
-			result+=(mc[i].price*amount);
+			/*result+=(mc[i].price*amount);*/
+			result+=(mc[i].sale*amount/(double)mc[i].amount);
 			break;
 		}
 		result+=mc[i].sale;
