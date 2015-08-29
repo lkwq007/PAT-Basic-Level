a0e48c3d7bf93a55bf82699c30b3dfa5104b1e82
Death <lkwq007@gmail.com>
2015-08-29 10:50:23 +0800

    Add 1025

---
+++ 1025-reverse-linked-list.c
@@ -0,0 +1,63 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+typedef struct
+{
+	char address[6];
+	char next[6];
+	int data;
+} list;
+int main()
+{
+	int n,k,i,j,value;
+	char start[6];
+	list *in,*out,temp;
+	scanf("%s%d%d",start,&n,&k);
+	in=(list *)malloc(n*sizeof(list));
+	out=(list *)malloc(n*sizeof(list));
+	for(i=0;i<n;i++)
+	{
+		scanf("%s%d%s",in[i].address,&in[i].data,in[i].next);
+		if((value=strcmp(start,in[i].address))==0)
+		{
+			out[0]=in[i];
+			strcpy(in[i].address,"F");
+		}
+	}
+	for(i=1;i<n;i++)
+	{
+		for(j=0;j<n;j++)
+		{
+			if((value=strcmp(out[i-1].next,in[j].address))==0);
+			{
+				out[i]=in[j];
+				break;
+			}
+		}
+	}
+	for(i=0;i<n;i+=k)
+	{
+		if(n-i>=k)
+		{
+			for(j=i;j<i+k/2;j++)
+			{
+				temp=out[j];
+				out[j]=out[i+k-j-1];
+				out[i+k-j-1]=temp;
+			}
+		}
+		else
+		{
+			break;
+		}
+	}
+	for(i=0;i<n-1;i++)
+	{
+		strcpy(out[i].next,out[i+1].address);
+		printf("%s %d %s\n",out[i].address,out[i].data,out[i].next);
+	}
+	strcpy(out[i].next,"-1");
+	printf("%s %d %s\n",out[i].address,out[i].data,out[i].next);
+	free(in);
+	free(out);
+}
