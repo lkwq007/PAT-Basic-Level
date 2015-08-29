26dc54dd584e29cd521f8300b4420be2d3d85488
Death <lkwq007@gmail.com>
2015-08-29 20:59:06 +0800

    Add 1030

---
+++ 1030-perfect-array.c
@@ -0,0 +1,47 @@
+#include<stdio.h>
+#include<stdlib.h>
+int compare(const void *a,const void *b)
+{
+	if(*(long *)a>*(long *)b)
+		return 1;
+	else if(*(long *)a<*(long *)b)
+		return -1;
+	else
+		return 0;
+}
+int main()
+{
+	long *num,*nums;
+	int i,j,n,p,max=1;
+	scanf("%d%d",&n,&p);
+	num=(long *)malloc(n*sizeof(long));
+	nums=(long *)malloc(n*sizeof(long));
+	for(i=0;i<n;i++)
+	{
+		scanf("%ld",num+i);
+	}
+	qsort(num,n,sizeof(long),compare);
+	for(i=0;i<n;i++)
+	{
+		nums[i]=num[i]*p;
+	}
+	for(i=0;i<n;i++)
+	{
+		if(n-i<max)
+			break;
+		for(j=n-1;j>=i;j--)
+		{
+			if(j-i<max)
+				break;
+			if(nums[i]>=num[j])
+			{
+				if(max<j-i+1)
+				{
+					max=j-i+1;
+				}
+				break;
+			}
+		}
+	}
+	printf("%d\n",max);
+}
